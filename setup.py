5c06566fec7d0d8b5f77f2c0ada50c7b12749f25
bill <billbsing@gmail.com>
2021-02-17 13:33:41 +0800

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -81,6 +81,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/datacraft-dsc/convex-contracts',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
